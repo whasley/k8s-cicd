19f332cfeac05ee1fba37482a602faadfcbc55eb
Whasley Cardoso <whasleysouza@gmail.com>
2022-05-07 17:08:13 -0300

    Creating  To Do Page

--- server.go
+++ server.go
@@ -1,13 +1,46 @@
 package main
 
-import "net/http"
+import (
+	"html/template"
+	"net/http"
+)
+
+type Todo struct {
+    Title string
+    Done  bool
+}
+
+type TodoPageData struct {
+    PageTitle string
+    Todos     []Todo
+}
+
 
 func main() {
+    tmpl := template.Must(template.ParseFiles("layout.html"))
+    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+        data := TodoPageData{
+            PageTitle: "My TODO list",
+            Todos: []Todo{
+                {Title: "Task 1", Done: false},
+                {Title: "Task 2", Done: true},
+                {Title: "Task 3", Done: true},
+            },
+        }
+        tmpl.Execute(w, data)
+    })
+    http.ListenAndServe(":3000", nil)
+}
 
-	http.HandleFunc("/", Home)
+
+
+/*func main() {
+
+http.HandleFunc("/", Home)
 	http.ListenAndServe(":3000", nil)
 }
 
 func Home(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("<h1>Olá mundo - Kubernetes</h1>"))
 }
+*/
