ecec2a536c74378be7b59660a34051c7a742185b
Whasley Cardoso <whasleysouza@gmail.com>
2022-05-11 15:55:50 -0300

    :0

--- server.go
+++ server.go
@@ -20,7 +20,7 @@ func main() {
     tmpl := template.Must(template.ParseFiles("layout.html"))
     http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
         data := TodoPageData{
-            PageTitle: "Projeto DevOps",
+            PageTitle: "Projeto DevOps - :0",
             Todos: []Todo{
                 {Title: "Git Push para o GitHub", Done: true},
                 {Title: "Login no DockerHub", Done: true},
